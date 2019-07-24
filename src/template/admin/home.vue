7e5a4fc2fb1696b5b56a796940806e6a96ed1281
NastyAir <1115094858@qq.com>
2019-07-24 23:25:47 +0800

    添加基础文件

---
+++ src/template/admin/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+
+        </header>
+        <div>
+            <sider></sider>
+            <router></router>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style scoped>
+
+</style>
